a6dc68bffd6f390d3ad0a400703593b7360a7495
0e1arquitetos <141720410+0e1arquitetos@users.noreply.github.com>
2024-12-11 15:55:49 -0300

    ICG - PB V01
    IBIS

---
+++ data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-corredor-01",
+      "name": "CORREDOR 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.0187958534830166,
+        "pitch": 0.013117983227099117,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.005468145153241011,
+          "pitch": 0.035316980209081805,
+          "rotation": 0,
+          "target": "1-corredor-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-corredor-02",
+      "name": "CORREDOR 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.05101768204066026,
+        "pitch": 0.003747995207742605,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.019936777883547308,
+          "pitch": 0.025937598650514815,
+          "rotation": 0,
+          "target": "2-corredor-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corredor-03",
+      "name": "CORREDOR 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.05504390515613977,
+        "pitch": 0.03935394968129735,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ICG - PB V01",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
